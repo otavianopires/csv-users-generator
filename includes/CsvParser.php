d5d17a4bcc6838442185198ae4f4dae3ed0e5ab1
Otaviano Pires <otavianopires@gmail.com>
2021-03-22 11:05:27 -0400

    Updated parser removing cunwanted characters and verify if user_login exists

--- includes/CsvParser.php
+++ includes/CsvParser.php
@@ -15,15 +15,20 @@ class CsvParser {
         if ( !file_exists($filename) ) {
             return false;
         }
-
         if ( ( $csv_file = fopen($filename, "r" ) ) !== FALSE ) {
 
-            $csv_headers = fgetcsv($csv_file, 1000, ",");
+            $first_line = fgetcsv($csv_file, 1000, ",");
+
+            $csv_headers = array();
+            for ($i = 0, $j = count($first_line); $i < $j; $i++) {
+                array_push($csv_headers, preg_replace('/[^(\x20-\x7F)]*/', '', $first_line[$i]) );
+            }
             
             while ( ( $csv_row = fgetcsv( $csv_file, 1000, "," ) ) !== FALSE ) {
+                $csv_row = array_map("utf8_encode", $csv_row);
+                $csv_row = array_combine( $csv_headers, $csv_row );
 
-                if ( isset( $csv_row ) && !empty( $csv_row[0] ) ) {
-                    $csv_row = array_combine( $csv_headers, $csv_row );
+                if ( $csv_row['user_login'] != '' ) {
                     array_push( $data_list, $csv_row );
                 }
             }
